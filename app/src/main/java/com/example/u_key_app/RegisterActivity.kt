33841beb2a52da096dad7e9228393491e0ba3aa7
KameHelen <elemesreq@gmail.com>
2026-04-11 11:40:03 +0200

    Creando loging y register

---
+++ app/src/main/java/com/example/u_key_app/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package com.example.u_key_app
+
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        // Soporte edge-to-edge (sin necesidad de ID personalizado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<ImageButton>(R.id.buttonBack).setOnClickListener {
+            finish() // Cierra RegisterActivity y vuelve a MainActivity
+        }
+
+    }
+
+}
